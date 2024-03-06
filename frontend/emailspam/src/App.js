e6193a9c9acc73153ad784ba1628aeb6d4b3c94f
Nirmal J13 <nirmalnj2003@gmail.com>
2024-03-06 23:33:41 +0530

    changed link

--- frontend/emailspam/src/App.js
+++ frontend/emailspam/src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   const checkspam=async()=>{
 
-  const senddata =await fetch('http://127.0.0.1:5000/',
+  const senddata =await fetch('https://email-spam.vercel.app/',
     {
       method:"POST",
       headers: {
